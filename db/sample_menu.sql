a9ad176bc7ac6db46e2b9db5784ca0270aefd47b
Omar <Omarggm@gmail.com>
2023-05-13 21:02:37 -0700

    sample data added to confirm database

--- db/sample_menu.sql
+++ db/sample_menu.sql
@@ -5,3 +5,15 @@ VALUES
 ('Item 3'),
 ('Item 4'),
 ('Item 5');
+
+SELECT * FROM sample_menu;
+
+INSERT INTO sample_menu2 (name)
+VALUES
+('Item 1'),
+('Item 2'),
+('Item 3'),
+('Item 4'),
+('Item 5');
+
+SELECT * FROM sample_menu2;
--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,17 @@
 DROP DATABASE IF EXISTS sample_db;
-
--- create database --
 CREATE DATABASE sample_db;
-
--- use database --
 USE sample_db;
 
---create table --
+
 CREATE TABLE IF NOT EXISTS `sample_menu` (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NULL,
-)
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS `sample_menu2` (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
